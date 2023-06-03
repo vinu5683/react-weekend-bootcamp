b159751e6530837479db8826d9f9332e80940e33
Vinod <vinodkumar.c@licious.com>
2023-06-03 22:57:12 +0530

    robothash.org page creation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import ListCardItem from "./ListCardItem.js"
 import reportWebVitals from './reportWebVitals';
+import 'tachyons'
+import {robots} from './robotresponse.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+    <ListCardItem id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <ListCardItem id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <ListCardItem id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    <ListCardItem id={robots[3].id} name={robots[3].name} email={robots[3].email}/>
+    <ListCardItem id={robots[4].id} name={robots[4].name} email={robots[4].email}/>
+    <ListCardItem id={robots[5].id} name={robots[5].name} email={robots[5].email}/>
+    </div>
   </React.StrictMode>
 );
 
